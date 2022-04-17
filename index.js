abca9b826684adbdf5b58ffa7ed616841e448561
difico123 <ducluong0915@gmail.com>
2022-04-17 12:52:13 +0700

    add redis cloud url

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.use(
 
 (async () => {
     const client = await createClient({
-        // url: process.env.REDIS_URL,
+        url: process.env.REDIS_URL,
     });
 
     client.on("error", (err) => console.log("Redis Client Error", err));
